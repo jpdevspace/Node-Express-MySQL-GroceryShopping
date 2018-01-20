cb1bc915bc5253378431baece3d08f63ed0aeb9f
jpwebdev <jjpwebdev@gmail.com>
2018-01-20 10:24:18 -0500

    Handlebars: Middleware setup, layout with Bootstrap linked, render basic view with data from backend test passed

--- app.js
+++ app.js
@@ -7,12 +7,21 @@ const exphbs  = require('express-handlebars');
 // Using Port 3000 or whatever the default port for the environment is
 const port = process.env.PORT || 3000;
 
+
 // Initializing Express app
 const app = express();
 
+
+// Middleware: Express-Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+const test = 'Indeed I am working!!!'
+
 // GET - Home Route
 app.get('/', (req, res) => {
-    res.send('working!');
+    res.render('index', { test });
 });
 
 // Starting Express Server
