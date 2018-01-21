69e4fe7a87d4863186178dc66eb0ed53218a264c
jpwebdev <jjpwebdev@gmail.com>
2018-01-20 20:43:43 -0500

    MySQL connection with  Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql');
 
+let connection;
 // MySQL Connection Setup
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "shopping_db" 
-});
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "shopping_db" 
+    });
+}
 
 // Start MySQL connection
 connection.connect( err => {
