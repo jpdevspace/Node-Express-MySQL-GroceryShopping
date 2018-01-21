fb8e412202079fca1a9b311beaaf2241f141862b
jpwebdev <jjpwebdev@gmail.com>
2018-01-21 10:59:20 -0500

    JawsDB setup with Heroku

--- db/shoppingSeed.sql
+++ db/shoppingSeed.sql
@@ -2,5 +2,8 @@ INSERT INTO shopping_table (item, person, purchased)
 VALUES 
 ("Milk", "Juli", false),
 ("Bread", "Juli", false),
+("Wine", "Carlos", true),
 ("Veggies", "Juli", true),
+("Coffee", "Alex", true),
+("A/C Filter", "JP", false),
 ("Eggs", "JP", false);
